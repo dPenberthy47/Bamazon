edfe3dbb820e1e270f5607c90b0d47e5a788bb9a
David Penberthy <davidpenberthy47@gmail.com>
2018-09-07 23:43:23 -0500

    stored responses in the variable successfully

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 
 let store;
+let sql;
+let total;
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -21,26 +23,12 @@ connection.connect((err, response) => {
 showMarket = () => {
   connection.query("SELECT * from products", (error, results) => {
     if (error) throw error;
-    console.log("\n--------------------------");
-    console.log("id |  name  |  dept  |  price  | quant ");
-    console.log("--------------------------\n");
 
-    for (i = 0; i < results.length; i++) {
-      console.log(
-        results[i].id +
-          " " +
-          results[i].name +
-          " | " +
-          results[i].dept +
-          " | " +
-          results[i].price +
-          " | " +
-          results[i].quant
-      );
-      store = results;
-    }
+    console.table(results);
+    store = results;
   });
 };
+
 questions = () => {
   inquirer
     .prompt([
@@ -67,12 +55,26 @@ questions = () => {
         message: "how many of that product would you like to purchase?"
       }
     ])
-    .then(answers => {
-      // Use user feedback for... whatever!!
-      storeChecker(answers);
+    // Use user feedback for... whatever!!
+    .then(function(answer) {
+      sql = "SELECT id, price, stock";
+      sql += "FROM products";
+      sql += "WHERE id = ?";
+      connection.query(sql, answer.item, (err, result) => {
+        if (answer.quanity > store[0].quant) {
+          console.log(
+            "We're sorry, but we do not have the sufficient quantities to fufill your order."
+          );
+        } else {
+          console.log(
+            "Thank you for shopping at our store. Your total for today is: $" +
+              total
+          );
+        }
+      });
     });
 };
-
+/*
 storeChecker = answers => {
   //needs to check the users input
   //compare to the input in the table
@@ -81,21 +83,9 @@ storeChecker = answers => {
       console.log(
         "Awesome! Here is your" + answers.quantity + " " + answers.name + "s"
       );
-    } else if (
-      answers.name === store[i].name &&
-      answers.quantity > store[i].quant
-    ) {
-      console.log(
-        "We're sorry, we do not have " +
-          answers.quantity +
-          " of our " +
-          answers.name +
-          ".\n Please come again soon!"
-      );
-    } else {
-      console.log(
-        "We're sorry, but we do not have that item yet. Go to Walmart or something."
-      );
     }
+
+    return console.log("sorry, we could not process your order");
   }
 };
+*/
